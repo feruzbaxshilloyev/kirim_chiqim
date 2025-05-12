88f82a8decf0e94aefbc1613cde4664a0dafd986
feruzbaxshilloyev <baxshilloyevferuz23@gmail.com>
2025-05-13 00:24:42 +0500

    1

--- home/views.py
+++ home/views.py
@@ -7,9 +7,6 @@ from profil.models import Profil
 
 
 def home_view(request):
-    # pr = Profil.objects.filter(user=request.user).first()
-    # if not request.user.is_authenticated:
-    #     return redirect('profil:login')
 
     today = datetime.today()
     haftalik_hisobot = []
